0d512405431f4f33796f2dc2714eaf28d6fc8f59
Josh <josh@josh-linux.(none)>
2014-06-10 02:31:42 +1200

    Bump PiPUB to v1.0.2

--- service.tinyhtpc.switch/bin/switch.sh
+++ service.tinyhtpc.switch/bin/switch.sh
@@ -19,10 +19,20 @@ power=$(cat /sys/class/gpio/gpio$GPIOpin1/value)
 else
 sleep 1
 if [[ $UID != 0 ]]; then
+    echo "0" > /sys/class/gpio/gpio$GPIOpin2/value
+    echo "Sending kill signal to PiPUB..."
+    sleep 1
+    echo "1" > /sys/class/gpio/gpio$GPIOpin2/value
     echo "Raspberry Pi shutting down..."
+    sleep 1
     sudo poweroff
 else
+    echo "0" > /sys/class/gpio/gpio$GPIOpin2/value
+    echo "Sending kill signal to PiPUB..."
+    sleep 1
+    echo "1" > /sys/class/gpio/gpio$GPIOpin2/value
     echo "Openelec shutting down..."
+    sleep 1
     poweroff
 fi
 fi
